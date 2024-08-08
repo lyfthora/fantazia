33821516e0f0009d7c941c919ee55cebfa14e8d8
Briarivero <marcosbrayan03@gmail.com>
2024-08-08 05:05:50 +0200

    login,registro y bbdd done.

---
+++ lain.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    header("Location: index.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Bienvenido, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
+</body>
+</html>
--- register.php
+++ register.php
@@ -2,21 +2,21 @@
 include 'db.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $username = $_POST['usuario'];
+    $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $confirm_password = $_POST['confirm_password']
+    $confirm_password = $_POST['confirm_password'];
 
     if ($password !== $confirm_password){
         die("Pass not same retard");
     }
-}
 
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+// recordar descomentar.
+// $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
 $sql = "INSERT INTO usuarios (username, email, password) VALUES (?,?,?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("sss", $username, $email, $hashed_password);
+$stmt->bind_param("sss", $username, $email, $password);
 
 if ($stmt->execute()) {
     echo "Registro exitoso.";
