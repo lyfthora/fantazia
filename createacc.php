b279023ce3ed1c565e63cfcc13ba15498bae616f
Briarivero <marcosbrayan03@gmail.com>
2024-08-08 16:13:05 +0200

    php

--- createacc.php
+++ createacc.php
@@ -11,9 +11,10 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styleslain.css" />
     <title>Document</title>
 </head>
 <body>
-<h1>Bienvenido, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
+<h1>Hello <?php echo htmlspecialchars($_SESSION['username']); ?>,</h1>
 </body>
 </html>
