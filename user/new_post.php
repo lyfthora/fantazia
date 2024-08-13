27410134c1819752efe3eab5d3aad735b59e47bc
Briarivero <marcosbrayan03@gmail.com>
2024-08-13 04:04:26 +0200

    css fix.

--- user/new_post.php
+++ user/new_post.php
@@ -18,7 +18,7 @@ $username = $_SESSION['username'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="/styles/stylesNewPost.css" />
+    <link rel="stylesheet" type="text/css" href="../styles/stylesNewPost.css" />
     <title>New Post</title>
 </head>
 <body>
